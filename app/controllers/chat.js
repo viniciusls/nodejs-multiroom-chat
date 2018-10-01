d8d8665d3573739950059734ecc6735679d13af9
Vinicius Silva <vinicius768@msn.com>
2018-10-01 14:51:26 -0300

    Implemented Socket.io

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,5 +1,6 @@
 module.exports.newChat = (application, req, res) => {
     const data = req.body;
+    const io = application.get('io');
 
     req.assert('username', 'Nome ou apelido é obrigatório.').notEmpty();
     req.assert('username', 'Nome ou apelido precisa ter entre 3 e 20 caracteres.').len(3, 20);
@@ -12,7 +13,8 @@ module.exports.newChat = (application, req, res) => {
         return;
     }
 
-    application.get('io').emit('newMsg', { username: data.username, msg: data.username + ' entrou na sala.' });
+    io.emit('newUser', { username: data.username });
+    io.emit('newMsg', { username: data.username, msg: data.username + ' entrou na sala.' });
 
     res.render('chat');
 };
