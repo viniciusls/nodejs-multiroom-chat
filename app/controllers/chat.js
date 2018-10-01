fa0fd13f63dec5145a43315eeff5592c845faa1a
Vinícius Silva <vinicius768@msn.com>
2018-10-01 01:12:28 -0300

    Added validator

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,5 +1,13 @@
 module.exports.newChat = (application, req, res) => {
     const data = req.body;
-    
+
+    req.assert('username', 'Nome ou apelido é obrigatório.').notEmpty().len(3, 100);
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        res.send(errors);
+    }
+
     res.render('chat');
 };
